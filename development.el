e5a75210350ead2258d267d31c4fa543360fed13
Roman Salmin <Roman.Salmin@gmail.com>
2024-02-09 15:03:01 +0300

    changed c style from linux to bsd

--- development.el
+++ development.el
@@ -2,7 +2,7 @@
 (defun custom-c-mode-hook ()
     ; always use spaces instead of tabs (use C-q TAB to insert tabs)
     (setq-default indent-tabs-mode nil)
-    (setq c-default-style "linux" c-basic-offset 4)
+    (setq c-default-style "bsd" c-basic-offset 4)
   )
 (add-hook 'c-mode-common-hook 'custom-c-mode-hook)
 
